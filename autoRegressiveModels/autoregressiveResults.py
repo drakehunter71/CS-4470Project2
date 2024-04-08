ce3811912faf6aa859188e79a8f4a95010532d01
Drake Smith <drake-smith@uiowa.edu>
2024-04-08 14:09:43 -0500

    Final commit

--- autoRegressiveModels/autoregressiveResults.py
+++ autoRegressiveModels/autoregressiveResults.py
@@ -2,29 +2,31 @@ import pandas as pd
 import os
 from sklearn.metrics import f1_score
 
-df = pd.read_csv("Data/combined.csv")
-df["data"] = df["data"].str.replace(r"[.\s]+$", "", regex=True)
-test_df = df[df["traintest"] == "test"].copy()
-test_df["speculative"] = (
-    test_df["speculative"]
-    .str.lower()
-    .str.replace(r"[.\s]+$", "", regex=True)
-    .map({"yes": 1, "no": 0})
-    .astype(int)
-)
-test_df.reset_index(drop=True, inplace=True)
+# df = pd.read_csv("Data/combined.csv")
+# df["data"] = df["data"].str.replace(r"[.\s]+$", "", regex=True)
+# test_df = df[df["traintest"] == "test"].copy()
+# test_df["speculative"] = (
+#     test_df["speculative"]
+#     .str.lower()
+#     .str.replace(r"[.\s]+$", "", regex=True)
+#     .map({"yes": 1, "no": 0})
+#     .astype(int)
+# )
+# test_df.reset_index(drop=True, inplace=True)
+test_df = pd.read_csv("Data/speculativeManualClassification.csv")
 
 # autoRegressiveModelResults = pd.read_csv("Results/autoRegressiveModelsUpdated.csv")
 autoRegressiveModelResults = pd.read_csv("Results/covidAutoregressiveModels.csv")
 
 
-autoRegressiveModelResults.loc[:, "type"] = test_df.loc[:, "type"]
-autoRegressiveModelResults.loc[:, "speculative"] = test_df.loc[:, "speculative"]
+# autoRegressiveModelResults.loc[:, "type"] = test_df.loc[:, "type"]
+# autoRegressiveModelResults.loc[:, "speculative"] = test_df.loc[:, "speculative"]
 
 models = ["GPT3.5", "GPT4", "Haiku", "Sonnet", "Opus"]
 types = ["tok", "stm", "bgm"]
 
-results_file_path = "Results/autoRegressiveMethodsf1.csv"
+# results_file_path = "Results/autoRegressiveMethodsf1.csv"
+results_file_path = "Results/covidAutoRegressiveMethodsf1.csv"
 file_exists = os.path.isfile(results_file_path)
 
 for model in models:
@@ -36,21 +38,34 @@ for model in models:
         .astype(int)
     )
 
-    for type in types:
-        filtered_df = autoRegressiveModelResults[
-            autoRegressiveModelResults["type"] == type
-        ]
+    y_true = test_df["speculative"]
+    y_pred = autoRegressiveModelResults[model]
 
-        y_true = filtered_df["speculative"]
-        y_pred = filtered_df[model]
+    test_f1_score = f1_score(y_true, y_pred)
 
-        test_f1_score = f1_score(y_true, y_pred)
+    results = {"method": model, "f1_score": test_f1_score}
 
-        results = {"method": model, "type": type, "f1_score": test_f1_score}
+    result_df = pd.DataFrame([results])
+    with open(
+        results_file_path, "a" if file_exists else "w", newline="", encoding="utf-8"
+    ) as f:
+        result_df.to_csv(f, header=not file_exists, index=False)
+    file_exists = True
+    # for type in types:
+    # filtered_df = autoRegressiveModelResults[
+    #     autoRegressiveModelResults["type"] == type
+    # ]
 
-        result_df = pd.DataFrame([results])
-        with open(
-            results_file_path, "a" if file_exists else "w", newline="", encoding="utf-8"
-        ) as f:
-            result_df.to_csv(f, header=not file_exists, index=False)
-        file_exists = True
+    # y_true = filtered_df["speculative"]
+    # y_pred = filtered_df[model]
+
+    # test_f1_score = f1_score(y_true, y_pred)
+
+    # results = {"method": model, "type": type, "f1_score": test_f1_score}
+
+    # result_df = pd.DataFrame([results])
+    # with open(
+    #     results_file_path, "a" if file_exists else "w", newline="", encoding="utf-8"
+    # ) as f:
+    #     result_df.to_csv(f, header=not file_exists, index=False)
+    # file_exists = True
--- speculativeDataResults.py
+++ speculativeDataResults.py
@@ -2,8 +2,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-traditionalMachineLearningF1 = pd.read_csv("Results/traditionalMethodsf1.csv")
-autoRegressiveModelF1 = pd.read_csv("Results/autoregressiveMethodsf1.csv")
+# traditionalMachineLearningF1 = pd.read_csv("Results/traditionalMethodsf1.csv")
+# autoRegressiveModelF1 = pd.read_csv("Results/autoregressiveMethodsf1.csv")
+traditionalMachineLearningF1 = pd.read_csv("Results/covidSpeculativef1.csv")
+autoRegressiveModelF1 = pd.read_csv("Results/covidAutoRegressiveMethodsf1.csv")
 
 f1_results = pd.concat([traditionalMachineLearningF1, autoRegressiveModelF1])
 f1_results.reset_index(drop=True, inplace=True)
@@ -12,17 +14,20 @@ f1_results.reset_index(drop=True, inplace=True)
 plt.figure(figsize=(10, 8))
 
 # Plot
-sns.barplot(x="method", y="f1_score", hue="type", palette="bright", data=f1_results)
-plt.title("F1 Score by Method and Type")
+# sns.barplot(x="method", y="f1_score", hue="type", palette="bright", data=f1_results)
+# plt.title("F1 Score by Method and Type")
+plt.title("F1 Score by Method")
+sns.barplot(x="method", y="f1_score", palette="bright", data=f1_results)
 plt.ylabel("F1 Score")
 plt.ylim(0, 1)
 plt.xlabel("Type")
 plt.xticks(rotation=45)
-plt.legend(title="Method", bbox_to_anchor=(1.05, 1), loc="upper left")
+# plt.legend(title="Method", bbox_to_anchor=(1.05, 1), loc="upper left")
 plt.tight_layout()
-plt.savefig("Results/f1_scores_plot.png")
+# plt.savefig("Results/f1_scores_plot.png")
+plt.savefig("Results/f1_scores_plot_speculative.png")
 plt.show()
-
+exit()
 for type in f1_results["type"].unique():
     sns.barplot(
         x="method",
